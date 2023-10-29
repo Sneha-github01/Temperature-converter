18265065b3c474f5b597172ce87e740518d1c139
Sneha-github01 <snehasatapathy8@gmail.com>
2023-10-29 10:51:35 +0530

    temp converters

---
+++ temp.js
@@ -0,0 +1,46 @@
+// Convertion from celsius to fahrenheit & kelvin
+
+let celsiusInput = document.querySelector('#celsius > input')
+let fahrenheitInput = document.querySelector('#fahrenheit > input')
+let kelvinInput = document.querySelector('#kelvin > input')
+let btn = document.querySelector('.button button')
+
+function roundNumber(number){
+    return Math.round(number*100)/100
+}
+
+// case1 celsius to fahrenheit & kelvin
+celsiusInput.addEventListener('input', function(){
+    let cTemp = parseFloat(celsiusInput.value)
+    let fTemp = (cTemp*(9/5))+32
+    let kTemp = cTemp + 273.15
+
+    fahrenheitInput.value = roundNumber(fTemp)
+    kelvinInput.value = roundNumber(kTemp)
+})
+
+// case2 fahrenheit to celsius & kelvin
+fahrenheitInput.addEventListener('input', function(){
+    let fTemp = parseFloat(fahrenheitInput.value)
+    let cTemp = (fTemp - 32) * (5/9)
+    let kTemp = (fTemp - 32) * (5/9) + 273.15
+
+    celsiusInput.value = roundNumber(cTemp)
+    kelvinInput.value = roundNumber(kTemp)
+})
+
+// case3 kelvin to celsius & fahrenheit
+kelvinInput.addEventListener('input', function(){
+    let kTemp = parseFloat(kelvinInput.value)
+    let cTemp = kTemp - 273.15
+    let fTemp = (kTemp - 273.15) * (9/5) + 32
+
+    celsiusInput.value = roundNumber(cTemp)
+    fahrenheitInput.value = roundNumber(fTemp)
+})
+
+btn.addEventListener('click', ()=>{
+    celsiusInput.value = "";
+    fahrenheitInput.value = "";
+    kelvinInput.value = "";
+})
